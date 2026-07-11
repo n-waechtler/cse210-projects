f314e7c12b3e18b39bc952042b9e29adf8d21718
n-waechtler <njwaechtler89@gmail.com>
2026-07-11 16:19:13 -0600

    coded journal class

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -1,24 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
 public class Journal
 {
-    public List<Entry> _entries;
+    public List<Entry> _entries = new List<Entry>();
 
     public void AddEntry(Entry newEntry)
     {
-
+        _entries.Add(newEntry);
     }
 
     public void DisplayAll()
     {
-
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
     }
 
     public void SaveToFile(string file)
     {
-
+        using (StreamWriter outputFile = new StreamWriter(file))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date}|{entry._promptText}|{entry._entryText}");
+            }
+        }
+        Console.WriteLine("Journal saved successfully!");
     }
-    
+
     public void LoadFromFile(string file)
     {
-        
+        _entries.Clear();
+
+        string[] lines = File.ReadAllLines(file);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("|");
+
+            Entry loadedEntry = new Entry();
+            loadedEntry._date = parts[0];
+            loadedEntry._promptText = parts[1];
+            loadedEntry._entryText = parts[2];
+
+            _entries.Add(loadedEntry);
+        }
+        Console.WriteLine("Journal loaded successfully!");  
     }
 }
