1919b4e070ecc8d2f90e5cf97bf8011ebe280eec
n-waechtler <njwaechtler89@gmail.com>
2026-07-18 10:00:14 -0600

    fixed missing code

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -7,7 +7,7 @@ class Program
         Reference reference = new Reference("Proverbs", 3, 5, 6);
 
         string fullText = "Trust in the Lord with all thine heart and lean not unto thine own understanding" +
-        "In all thy ways acknowledge him and he shall direct thy paths";
+                          "In all thy ways acknowledge him and he shall direct thy paths";
 
         Scripture scripture = new Scripture(reference, fullText);
 
@@ -18,6 +18,14 @@ class Program
             Console.WriteLine();
 
             if (scripture.IsCompletelyHidden())
+            {
+                break;
+            }
+
+            Console.WriteLine("Press Enter to continue or type 'quit' to finish:");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
             {
                 break;
             }
