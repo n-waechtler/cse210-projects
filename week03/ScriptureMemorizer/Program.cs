9fc7a35a43f9a71e5b5c19438bdd44796fc74699
n-waechtler <njwaechtler89@gmail.com>
2026-07-18 09:53:12 -0600

    created program class

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+
+        string fullText = "Trust in the Lord with all thine heart and lean not unto thine own understanding" +
+        "In all thy ways acknowledge him and he shall direct thy paths";
+
+        Scripture scripture = new Scripture(reference, fullText);
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine();
+
+            if (scripture.IsCompletelyHidden())
+            {
+                break;
+            }
+
+            scripture.HideRandomWords(3);
+        }
+
+        Console.WriteLine("\nGoodbye!");
     }
 }
