016d14ff316962bdb0af49c5cf2604f442eef157
n-waechtler <njwaechtler89@gmail.com>
2026-07-25 18:19:01 -0600

    finished online ordering program

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -23,17 +23,18 @@ class Program
         Customer customer2 = new Customer("Fred Warner", address2);
         Order order2 = new Order(customer2);
 
-        Product p3 = new Product("Hatchet", "H310", 29.99, 3);
-        Product p4 = new Product("Fire Starter", "F45", 9.99, 5);
+        Product p3 = new Product("Helmet", "H630", 79.99, 3);
+        Product p4 = new Product("Mountain Bike", "B510", 699.99, 2);
+        Product p5 = new Product("Bike Pump", "P28", 39.99, 4);
 
         order2.AddProduct(p3);
         order2.AddProduct(p4);
         order2.AddProduct(p5);
 
-        Console.WriteLine("=============== Order 1 ===============");
-        Console.WriteLine(order1.GetPackingLabel());
-        Console.WriteLine(order1.GetShippingLabel());
-        Console.WriteLine($"Total Price: ${order1.CalculateOrderTotal():F2}\n");
+        Console.WriteLine("=============== Order 2 ===============");
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order2.CalculateOrderTotal():F2}\n");
 
 
 
