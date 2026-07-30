26c2c016c79a1a2ca55bf2a5462238bb47b2ce63
n-waechtler <njwaechtler89@gmail.com>
2026-07-29 22:39:33 -0600

    set up classes, created Activity class

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace MindfulnessProgram
+{
+    public abstract class Activity
+    {
+        protected string _name;
+        protected string _description;
+        protected int _duration;
+
+        public Activity(string name, string description)
+        {
+            _name = name;
+            _description = description;
+        }
+
+        public void DisplayStartingMessage()
+        {
+            Console.Clear();
+            Console.WriteLine($"Welcome to the {_name}.\n");
+            Console.WriteLine($"{_description}\n");
+            Console.Write("How long, in seconds, would you like for your session? ");
+
+            while (!int.TryParse(Console.ReadLine(), out _duration) || _duration <= 0) ;
+            {
+                Console.Write("Please enter a valid positive number of seconds: ");
+            }
+
+            Console.Clear();
+            Console.WriteLine("Get ready...");
+            ShowSpinner(3);
+            Console.WriteLine();
+        }
+
+        public void DisplayEndingMessage()
+        {
+            Console.WriteLine("\nWell done!!");
+            ShowSpinner(3);
+            Console.WriteLine($"You have completed another {_duration} seconds of the {_name}.");
+            ShowSpinner(4);
+        }
+
+        protected void ShowSpinner(int seconds)
+        {
+            List<string> animationStrings = new List<string> { "|", "/", "-", "\\" };
+            DateTime startTime = DateTime.Now;
+            DateTime endTime = startTime.AddSeconds(seconds);
+
+            int i = 0;
+            while (DateTime.Now < endTime)
+            {
+                string s = animationStrings[i];
+                Console.Write(s);
+                Thread.Sleep(250);
+                Console.Write("\b \b");
+                i++;
+                if (i >= animationStrings.Count)
+                {
+                    i = 0;
+                }
+            }
+        }
+
+        protected void ShowCountdown(int seconds)
+        {
+            for (int i = seconds; i > 0; i--)
+            {
+                Console.Write(i);
+                Thread.Sleep(1000);
+                Console.Write("\b \b");
+            }
+        }
+
+        public abstract void Run();
+    }
+}
