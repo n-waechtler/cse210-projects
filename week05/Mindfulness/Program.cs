9762005c7d80f795f4835c4fb0c540f8e22b02c2
n-waechtler <njwaechtler89@gmail.com>
2026-07-30 20:57:19 -0600

    Created main program

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using MindfulnessProgram;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        BreathingActivity breathing = new BreathingActivity();
+        ReflectionActivity reflection = new ReflectionActivity();
+        ListingActivity listing = new ListingActivity();
+
+        string choice = "";
+
+        while (choice != "4")
+        {
+            Console.Clear();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine(" 1. Start breathing activity");
+            Console.WriteLine(" 2. Start reflection activity");
+            Console.WriteLine(" 3. Start listing activity");
+            Console.WriteLine(" 4. Quit");
+            Console.Write("Select a choice from the menu: ");
+
+            choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    breathing.Run();
+                    break;
+                case "2":
+                    reflection.Run();
+                    break;
+                case "3":
+                    listing.Run();
+                    break;
+                case "4":
+                    Console.WriteLine("\nGoodbye!");
+                    break;
+                default:
+                    Console.WriteLine("\nInvalid option. Press Enter to try again.");
+                    Console.ReadLine();
+                    break;
+            }
+        }
     }
 }
