7a67a836fc058bc067bf28fcbea3347fcd568aaa
n-waechtler <njwaechtler89@gmail.com>
2026-08-06 22:35:14 -0600

    finished coding checklist goal class

---
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -0,0 +1,41 @@
+public class ChecklistGoal : Goal
+{
+    private int _amountCompleted;
+    private int _target;
+    private int _bonus;
+
+    public ChecklistGoal(string name, string description, int points, int target, int bonus, int amountCompleted = 0)
+        : base(name, description, points)
+    {
+        _target = target;
+        _bonus = bonus;
+        _amountCompleted = amountCompleted;
+    }
+
+    public override int RecordEvent()
+    {
+        if (_amountCompleted < _target)
+        {
+            _amountCompleted++;
+            if (_amountCompleted == _target)
+            {
+                return GetPoints() + _bonus;
+            }
+            return GetPoints();
+        }
+        return 0;
+    }
+
+    public override bool IsComplete() => _amountCompleted >= _target;
+
+    public override string GetDetailsString()
+    {
+        string statusSign = IsComplete() ? "X" : " ";
+        return $"[{statusSign}] {GetName()} ({GetDescription()}) - Currently completed: {_amountCompleted}/{_target}";
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"CheckListGoal:{GetName()},{GetDescription()},{GetPoints()},{_bonus},{_target},{_amountCompleted}";
+    }
+}
