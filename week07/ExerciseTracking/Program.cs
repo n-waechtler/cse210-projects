d05f88a66d8cb8d28d4498b59437420c1b41cd15
n-waechtler <njwaechtler89@gmail.com>
2026-08-10 20:26:43 -0600

    Coded main program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running("03 Nov 2022", 30, 3.0));
+        activities.Add(new Cycling("03 Nov 2022", 45, 15.0));
+        activities.Add(new Swimming("04 Nov 2022", 40, 40));
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
