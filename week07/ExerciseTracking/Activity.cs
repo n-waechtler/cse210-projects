f4147eac5729a073cebccaa93e0af081c3294ae9
n-waechtler <njwaechtler89@gmail.com>
2026-08-10 20:09:08 -0600

    Created new class files, coded Activity class

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,35 @@
+using System;
+
+public abstract class Activity
+{
+    private string _date;
+    private int _minutes;
+
+    public Activity(string date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    protected int GetMinutes()
+    {
+        return _minutes;
+    }
+
+    protected string GetDate()
+    {
+        return _date;
+    }
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{_date} {this.GetType().Name} ({_minutes} min)- " +
+            $"Distance {GetDistance():F1} miles, " +
+            $"Speed {GetSpeed():F1} mph, " +
+            $"Pace: {GetPace():F1} min per mile";
+    }
+}
